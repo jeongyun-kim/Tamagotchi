1fe2f64131ed36fda0df2982132a807bde59099b
jeongyun-kim <kkanyo04@naver.com>
2024-06-08 00:43:42 +0900

    Initial Commit

---
+++ Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by 김정윤 on 6/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
